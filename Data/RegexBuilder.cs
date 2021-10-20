c7026a7fc50df13e865294da35a333d0bccae00e
Brian Donaldson <brian@bitobrian.com>
2021-10-20 17:24:40 -0500

    init

---
+++ Data/RegexBuilder.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Bitobrian.RegexMaker.Web.Data
+{
+    public static class RegexBuilder
+    {
+        public static string ToRegex(string[] valueArray, string currentRegex)
+        {
+            var finalRegex = new StringBuilder(currentRegex);
+
+            //1st Section
+            switch (valueArray[0])
+            {
+                case "Starts With":
+                    if (currentRegex != "")
+                        break;
+                    finalRegex.Append(@"(?<!(.))");
+                    break;
+            }
+
+            //2nd Section
+            switch (valueArray[1])
+            {
+                case "Any Character":
+                    finalRegex.Append(@".");
+                    break;
+                case "Any Integer":
+                    finalRegex.Append(@"[0-9]");
+                    break;
+                case "Any Letter":
+                    finalRegex.Append(@"[a-zA-Z]");
+                    break;
+                case "Any Lower Cased Letter":
+                    finalRegex.Append(@"[a-z]");
+                    break;
+                case "Any Upper Cased Letter":
+                    finalRegex.Append(@"[A-Z]");
+                    break;
+                case "Any Alphanumeric":
+                    finalRegex.Append(@"[0-9a-zA-Z]");
+                    break;
+                case "String":
+                    finalRegex.Append(valueArray[2].Contains('.') ? @"\." : valueArray[2]);
+                    break;
+                case "Specify":
+                    finalRegex.Append(valueArray[2].Contains('.') ? @"\." : valueArray[2]);
+                    break;
+            }
+
+            if (valueArray[1].Equals("String"))
+            {
+                return finalRegex.ToString();
+            }
+            
+            switch (valueArray[3])
+            {
+                case "Once":
+                    finalRegex.Append(@"{1}");
+                    break;
+                case "One or More":
+                    finalRegex.Append(@"{1,}");
+                    break;
+                case "Between..":
+                    finalRegex.Append(@"{"+valueArray[4]+","+valueArray[5]+"}");
+                    break;
+            }
+            
+            return finalRegex.ToString();
+        }
+        
+        public static string ToFinalRegex(string regex)
+        {
+            var capRegex = @"(?!.)";
+
+            var currentRegex = new StringBuilder(regex);
+
+            if(!regex.Contains(capRegex))
+                currentRegex.Append(capRegex);           
+
+            return currentRegex.ToString();
+        }
+        
+        public static bool CheckRegex(string inputValue, string regexValue)
+        {
+            var matchSuccess = false;
+
+            try
+            {
+                var regex1 = new Regex(regexValue);
+                var match = regex1.Match(inputValue);
+
+                if (match.Success)
+                    matchSuccess = true;
+            }
+            catch
+            {
+            }
+
+
+            return matchSuccess;
+        }
+    }
+}
---
+++ Data/RegexMakerVM.cs
@@ -0,0 +1,51 @@
+﻿namespace Bitobrian.RegexMaker.Web.Data
+{
+    public class RegexMakerVM
+    {
+        public string RegexString { get; set; } = "";
+        public string BuildSelected { get; set; } = "Starts With";
+        public string CharacterSelected { get; set; } = "Any Character";
+        public string OccurrenceSelected { get; set; } = "Once";
+        public string TestString { get; set; } = "";
+        public string SpecificCharacter { get; set; } = "";
+        public SpecificOccurrence SpecificOccurrence { get; set; } = new();
+        public string HistoryLog { get; set; } = "";
+
+        public string[] BuildSelection =
+        {
+            "Starts With", 
+            "Contains"
+        };
+        
+        public string[] CharacterTypeSelection =
+        {
+            "Any Character", 
+            "Any Integer",
+            "Any Letter", 
+            "Any Lower Cased Letter", 
+            "Any Upper Cased Letter", 
+            "Any Alphanumeric",
+            "String",
+            "Specify", 
+        };
+        
+        public string[] OccurrenceSelection =
+        {
+            "Once", 
+            "One or More", 
+            "Between.."
+        };
+    
+        public bool TestPass { get; set; }
+
+        public string LogMessage { get; set; } = "";
+        public bool ShowCharacterInput = false;
+        public bool ShowOccurrenceInput = false;
+    }
+
+    public class SpecificOccurrence
+    {
+        public string First { get; set; } = "";
+        public string Last { get; set; } = "";
+    }
+}
